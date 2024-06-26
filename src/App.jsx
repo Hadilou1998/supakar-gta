087f1ae2bb1d1e4849a7729a9cc7538606c7fbe9
your_github_Hadilou1998 <your_github_hadilissilame@gmail.com>
2024-06-26 09:38:26 +0200

    New Project supakar-gta

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,74 @@
 import { useState } from "react";
 import supakarLogo from "../public/logo.png";
-import loading from "../public/sounds/loading.mp3";
+
+// Cars color
+import brownCar from "../public/cars/brown.png";
+import grayCar from "../public/cars/gray.png";
+import greenCar from "../public/cars/green.png";
+import purpleCar from "../public/cars/purple.png";
+import skyCar from "../public/cars/skyblue.png";
+import yellowCar from "../public/cars/yellow.png";
+
+// Sounds
+import loadingSound from "../public/sounds/loading.mp3";
+import selectSound from "../public/sounds/select.wav";
+import spraySound from "../public/sounds/spray.wav";
 
 function App() {
-  const [instruction, setInstruction] = useState("Click on START to begin.");
-  const audio = new Audio(loading);
+  const [CarImage, setCarImage] = useState(yellowCar);
 
-  const start = () => {
-    audio.play();
+  const colors = [
+    {
+      name: "brown",
+      image: brownCar,
+    },
+    {
+      name: "gray",
+      image: grayCar,
+    },
+    {
+      name: "green",
+      image: greenCar,
+    },
+    {
+      name: "yellow",
+      image: yellowCar,
+    },
+    {
+      name: "sky",
+      image: skyCar,
+    },
+    {
+      name: "purple",
+      image: purpleCar,
+    },
+  ];
+
+  const loading = new Audio(loadingSound);
+  const select = new Audio(selectSound);
+  const spray = new Audio(spraySound);
+  /**
+  Lancer un son
+  audio.play();
+  */
+
+  /**
+  Mettre en place un delai en ms
+    setTimeout(() => {}, 1300);
+ */
+
+  const changeColor = (e) => {
+    e.preventDefault();
+    const color = e.target.getAttribute("data-color");
+    console.log(color);
+    select.play();
     setTimeout(() => {
-      setInstruction(
-        'Supakar est un projet pour l\'apprentissage de React. Il est basé sur le template Vite + React. Pour démarrer, il suffit de lire le fichier "README.md". Bonne session de codage !',
-      );
-    }, 1300);
+      spray.play();
+    }, 500);
+    setTimeout(() => {
+      setCarImage(
+        colors.find((c) => c.name === color).image);
+    }, 900);
   };
 
   return (
@@ -20,7 +76,7 @@ function App() {
       <div
         className="p-5 flex flex-col items-center justify-center h-screen"
         style={{
-          backgroundImage: "url('/cars/gray.png')",
+          backgroundImage: "url('" + CarImage + "')",
           backgroundSize: "cover",
           backgroundRepeat: "no-repeat",
         }}
@@ -28,15 +84,18 @@ function App() {
         <div className="p-10 flex flex-col items-center justify-center shadow-2xl rounded-2xl w-96 bg-white bg-opacity-70">
           <div className="w-64 mb-10">
             <img src={supakarLogo} alt="Supakar Logo" />
-          </div>
-          <button
-            onClick={start}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mb-5 rounded"
-          >
-            START
-          </button>
-          <div className="text-center text-lg transitison duration-300">
-            {instruction}
+            {colors.map((color) => (
+              <button
+                key={color.name}
+                data-color={color.name}
+                className={
+                  "bg-" + color.name + "-500 text-white p-2 text-xl rounded-md"
+                }
+                onClick={changeColor}
+              >
+                {color.name}
+              </button>
+            ))}
           </div>
         </div>
       </div>
